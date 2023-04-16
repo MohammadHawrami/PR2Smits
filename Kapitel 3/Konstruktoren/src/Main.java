b5e7e8c466cf84590cb47dc389a0ca83b085f4c4
Mohammad <Mohammad.mh811@gmail.com>
2023-04-16 20:54:53 +0200

    Anfang zur dritten Studienleistung

---
+++ Kapitel 3/Konstruktoren/src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    /*
+    -Konstruktor name muss immer gleich von Klassen name sein
+    -initialisieren Objekte bei erzeugung (ist da um neue Objekte erstellen zu koennen)
+    -keine vererbung und keine Methoden
+    -kann ueberladen werden
+    -gegenseitiger aufruf sowohl von der Superklass
+
+    -aufruf von anderen Konstruktoren in selber Klass = this.name
+    -aufruf von Konstruktor von Superklasse = super.name
+
+    Default-Konstruktor:
+    -default Konstruktor = Konstruktor ohne Parameter
+    -wenn in einem Programm es keinen Konstruktor gibt, dann erstellt der compiler von sich selber einen
+    default-Konstruktor, wenn aber einer dazu kommt, dann wird keine default Konstruktor automatisch erstellt
+
+    Aufruf des Superklassen-Konstruktors:
+    -Aufruf von Konstruktur auf Superklasse mit super.name
+    -super.name muss in erster zeile angegeben werden
+    -wenn in Elternklasse es keine Default-Konstruktor gibt, dann kommt es zu Compiler-Fehler
+    -wenn alle Konstruktoren = private, dann kann man davon nicht ableiten und auch keine Objekte erstellen
+
+
+    Aufrufhierarchie von Konstruktoren:
+
+     */
+}
