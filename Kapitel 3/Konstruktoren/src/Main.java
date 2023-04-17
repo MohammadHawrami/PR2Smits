54cb1621325ef93d8ffe6a57cf4da49a21d4f98d
Mohammad <Mohammad.mh811@gmail.com>
2023-04-17 14:34:57 +0200

    Anfang zur dritten Studienleistung

--- Kapitel 3/Konstruktoren/src/Main.java
+++ Kapitel 3/Konstruktoren/src/Main.java
@@ -22,6 +22,29 @@ public class Main {
 
 
     Aufrufhierarchie von Konstruktoren:
+    -hat expilzit einen this und delegiert anderen Konstruktor der selben klasse
+    -hat expilzit ein super und delegiert anderen Konstruktur von Superklasse
+    -nichts geschrieben = Compiler erzeugt automatisch ein super
+    -ein Konstruktor muss kein this enthalten sonst = Kreislauf
+    -durch this und super wird sicher gestellt das alle Objekte korret initiallisiert wurden
+    -bevor der eingen Konstruktor in der Superklasse aufgerufen wird, wird erst das mit dem this oder super aufgerufen
+    -geerbte Variablen werden vor eigene Variablen initiallisiert und vor eigenen Anweisung durchgefuehrt
+
+    Copy-Konstruktor:
+    -initialisiert Objekt auf ein bereits basierendes Objekt
+    -durch Copy-Konstruktor kann man weitere Objekte erzeugen und diese nachtraeglich anpassen
+
+    Unveraenderliche Objekte:
+    -Zustand von Objekt kann nach erzeugung nicht veraendert werden
+    -Nur Konstruktor setzt den Zustand und keine ander Methode
+    Vorteile:
+        -irrelevant ob zwei Refernezen auf zwei gleiche oder identische Objekte zeigen
+        -eigene Optimierung moeglich
+    Nachteile:
+        -Handhabung kompliziert
+        -Performance kann leiden
+   -immutable Objekte koennen in einigen Faellen zu Performance problemen fuehren
+   -ersetzen von zwei gleiche und unveraenderliche Objekt = interning
 
      */
 }
